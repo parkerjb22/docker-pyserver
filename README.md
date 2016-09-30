cbad8f72e46ab84f2c13cf734a768e4ab66cb298
parkerjb22 <parkerjb@gmail.com>
2016-09-30 08:45:20 -0500

    adding readme and docker install script

---
+++ README.md
@@ -0,0 +1,4 @@
+-- chmod u+x dockerinstall.sh
+-- ./dockerinstall.sh
+-- docker build -t pyserver .
+-- docker run -d -p 8082:8082 --name pysever --privileged pyserver
---
+++ dockerinstall.sh
@@ -0,0 +1,6 @@
+sudo apt-get install -y apt-transport-https
+wget -q https://packagecloud.io/gpg.key -O - | sudo apt-key add - 
+echo 'deb https://packagecloud.io/Hypriot/Schatzkiste/debian/ wheezy main' | sudo tee /etc/apt/sources.list.d/hypriot.list
+sudo apt-get update
+sudo apt-get install -y docker-hypriot
+sudo systemctl enable docker
